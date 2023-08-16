4a0ccd0f0deb717ca13a73ae64f4961d711ed6cb
Jovanny Sebastião <sebastiaojovanny@gmail.com>
2023-08-16 04:29:23 +0100

    Adicionado o darkMode e salvo no localStore do navegador

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import hamburgerMenu from "./sessões/menu_hamburguesa.js";
 import { relogioDigital,alarme } from "./sessões/relogio_digital.js";
 import { moveBall, shortcuts } from "./sessões/eventos_teclado.js";
 import countdown from "./sessões/contagem_regressiva.js";
+import scrollTopButton from "./sessões/botton_scroll.js";
+import darkMode from "./sessões/dark_mode.js";
 
 
 const d = document;
@@ -14,11 +16,13 @@ d.addEventListener("DOMContentLoaded", (e) =>{
     countdown(
      "countdown",
      "January 01, 2024 00:00:00",
-     "Feliz Ano Novo!!")
-});
-
-d.addEventListener("keydown", (e) => {
-    shortcuts(e);
-    moveBall(e, ".ball", ".stage")
-});
- 
+     "Feliz Ano Novo!!");
+     scrollTopButton(".scroll-top-btn");
+    });
+    
+    d.addEventListener("keydown", (e) => {
+        shortcuts(e);
+        moveBall(e, ".ball", ".stage")
+    });
+    
+    darkMode(".dark-theme-btn", "dark-mode");
